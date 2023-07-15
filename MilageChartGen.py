dbe570439f2e55c4828f3e0c867a6e8da72f0995
Matthew DiMaggio <63763143+Echoos1@users.noreply.github.com>
2023-07-15 03:02:56 -0400

    Finished Chart Generation

--- MilageChartGen.py
+++ MilageChartGen.py
@@ -1,4 +1,5 @@
 try:
+    import math
     import gpxpy
     import gpxpy.gpx
 except ModuleNotFoundError:
@@ -20,52 +21,9 @@ gpx_file = open(gpxfile, 'r')
 gpx = gpxpy.parse(gpx_file)
 
 
-def findSourceSegment(source, legs):
-    hits = []
-    for segment in legs:
-        if source == legs[segment][0].name:
-            hits.append(segment)
-    return hits
-
-
-def findNextSegments(srcSegment, legs):
-    hits = []
-    srcSegmentEnd = [legs[srcSegment][-1].latitude,legs[srcSegment][-1].longitude]
-    for segment in legs:
-        if srcSegmentEnd == [legs[segment][0].latitude,legs[segment][0].longitude]:
-            if legs[srcSegment] == list(reversed(legs[segment])):
-                pass
-            else:
-                hits.append(segment)
-    return hits
-
-
-def checkForEnd(destination, legs, checkSegment):
-    for segment in legs:
-        if destination == legs[checkSegment][-1].name:
-            if [legs[segment][0].latitude,legs[segment][0].longitude] \
-                == [legs[checkSegment][0].latitude,legs[checkSegment][0].longitude]:
-                    return True
-    return False
-
-
-legs = {}
-pois = {}
-
-for track in gpx.tracks:
-    points = []
-    seg_count = 0
-    for segment in track.segments:
-        legs[f'{track.name} Seg {seg_count}'] = segment.points
-        legs[f'{track.name} Seg {seg_count} Rev'] = list(reversed(segment.points))
-        seg_count += 1
-        for point in segment.points:
-            if point.name is not None and point.name not in pois:
-                pois[point.name] = point
-                pois[point.name]
-    
-
 def pathfind(frompoi, topoi):
+    if frompoi == topoi:
+        return {'Distance': 0, 'Path': None} 
     routes = {}
     success = []
     dead = []
@@ -115,23 +73,103 @@ def pathfind(frompoi, topoi):
         for step in solutions[path]:
             for track in gpx.tracks:
                 if f'{track.name} ' in step:
-                    distance += track.length_2d()/1609.344
+                    distance += track.length_3d()/1609.344
 
         distances.append(distance)
         
-    print(f'Dist: {min(distances)} miles \n {solutions[distances.index(min(distances))]}')
-                    
-                    
-pathfind("Munising Falls Visitor Center", "Woodland Park Campground")
+    return {'Distance': min(distances), 
+            'Path': solutions[distances.index(min(distances))]}
+   
 
-"""
-pathnum = 0            
-for path in success:
-    pathnum += 1
-    stepcount = 0
-    print(f'Solution {pathnum}')
-    for step in routes[path]:
-        stepcount += 1
-        print(f'{stepcount}. {step}')
-    print("\n")
-        """
+def findSourceSegment(source, legs):
+    hits = []
+    for segment in legs:
+        if source == legs[segment][0].name:
+            hits.append(segment)
+    return hits
+
+
+def findNextSegments(srcSegment, legs):
+    hits = []
+    srcSegmentEnd = [legs[srcSegment][-1].latitude,legs[srcSegment][-1].longitude]
+    for segment in legs:
+        if srcSegmentEnd == [legs[segment][0].latitude,legs[segment][0].longitude]:
+            if legs[srcSegment] == list(reversed(legs[segment])):
+                pass
+            else:
+                hits.append(segment)
+    return hits
+
+
+def checkForEnd(destination, legs, checkSegment):
+    for segment in legs:
+        if destination == legs[checkSegment][-1].name:
+            if [legs[segment][0].latitude,legs[segment][0].longitude] \
+                == [legs[checkSegment][0].latitude,legs[checkSegment][0].longitude]:
+                    return True
+    return False
+
+
+legs = {}
+pois = {}
+
+for track in gpx.tracks:
+    points = []
+    seg_count = 0
+    for segment in track.segments:
+        legs[f'{track.name} Seg {seg_count}'] = segment.points
+        legs[f'{track.name} Seg {seg_count} Rev'] = list(reversed(segment.points))
+        seg_count += 1
+        for point in segment.points:
+            if point.name is not None and point.name not in pois:
+                pois[point.name] = point
+                pois[point.name]
+
+unsortedPoiNames = []
+unsortedPoiLocs = []
+for poi in pois:
+    unsortedPoiNames.append(pois[poi].name)
+    unsortedPoiLocs.append(pois[poi].longitude)
+
+sortedPoiLocs = sorted(unsortedPoiLocs)
+sortedPoiNames = []
+for locs in sortedPoiLocs:
+    sortedPoiNames.append(unsortedPoiNames[unsortedPoiLocs.index(locs)])
+
+print("Writing to file...")
+totalToDo = len(pois) ** 2
+totalDone = 0
+with open("MilageChart.csv", "w") as csv:
+    topline = ["",]
+    for frompoi in sortedPoiNames:
+        topline.append(frompoi)
+    for cell in topline:
+        csv.write(f'{cell},')
+    csv.write("\n")
+    for frompoi in sortedPoiNames:
+        csv.write(f'{frompoi},')
+        timemode = 0
+        for topoi in sortedPoiNames:
+            distance = pathfind(frompoi, topoi)["Distance"]
+            if timemode == 0:
+                if distance == 0:
+                    timemode = 1
+                csv.write(f'{distance},')
+                
+            else:
+                # time = distance / speed
+                # speed is affected by 2.5% per degree in slope
+                avgSpeed = 2.5  #mph
+                elevationChange = (pois[topoi].elevation - pois[frompoi].elevation)/1609.344
+                slope = math.degrees(math.atan2(elevationChange,distance))
+                percentChange = 2.5 * slope
+                newSpeed = ((avgSpeed*percentChange)/100)+avgSpeed
+                time = distance / newSpeed
+                csv.write(f'{time},')
+                
+            totalDone += 1
+            print(f'[{"█"*int(((totalDone/totalToDo)*10))}{"-"*int((10-((totalDone/totalToDo)*10)))}] {(totalDone/totalToDo)*100:.02f}%   ', end="\r")
+        csv.write("\n")
+        
+        
+    print("[██████████] Done!        ")
